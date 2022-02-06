ae24967fc15c167d5f34250edcd12d1fd08e2319
mahfoo75 <mahfooz.lakhani@gmail.com>
2022-02-06 08:41:05 +0530

    Initial Commit

---
+++ src/main/java/com/selftech/springbootreactivepoc/SpringbootReactivePocApplication.java
@@ -0,0 +1,13 @@
+package com.selftech.springbootreactivepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactivePocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootReactivePocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/selftech/springbootreactivepoc/SpringbootReactivePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selftech.springbootreactivepoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactivePocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
